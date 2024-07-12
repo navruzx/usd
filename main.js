86200554ae8ad0c48005cc15897c9a1f42db576a
navruzx <nnavruz095@gmail.com>
2024-07-12 10:58:55 -0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,235 @@
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// let amal = prompt("amalni kkirit");
+// switch (amal) {
+//     case "+" :
+//     break;
+//     case "-":
+//         alert( a + b );
+//         break;
+//         case "-":
+//             alert(a - b);
+//             break;
+//             case "/":
+//                 alert(a / b);
+//                 break;
+//                 case "*":
+//                     alert(a * b);
+//                     break;
+//                     default:
+//                         alert("Bunaqa amal mavjud emas");
+// }
+// let day = +prompt("Hafta kunini kirit");
+// switch (day) {
+//     case 1:
+//         alert('Dushanba');
+//         break
+//         case 2:
+//             alert('Seshanba');
+//             break
+//             case 3:
+//                 alert('Chorshanba');
+//                 break
+//                 case 4:
+//                     alert('Payshanba');
+//                     break
+//                     case 5:
+//                         alert('Juma');
+//                         break
+//                         case 6:
+//                             alert('Shanba');
+//                             break
+//                             case 7:
+//                                 alert('Yakshanba');
+//                                 break
+//                                 default :
+//                                 alert("Bunaqa hafta kuni yuq")
+            
+// }
+// let day = +prompt("Son kirit");
+// switch (day) {
+//     case 1:
+//         alert('Yomon');
+//         break
+//         case 2:
+//             alert('qoniqarsiz');
+//             break
+//             case 3:
+//                 alert('qoniqarli');
+//                 break
+//                 case 4:
+//                     alert('Yaxshi');
+//                     break
+//                     case 5:
+//                         alert("A'lo");
+//                         break
+//                                 default :
+//                                 alert("Bunaqa baho  yuq")
+            
+// }
+// let month = +prompt("Son kirit");
+// switch (month) {
+//     case 1:
+//         alert('Yanvar qish');
+//         break
+//         case 2:
+//             alert('Fevral qish');
+//             break
+//             case 3:
+//                 alert('Mart bahor');
+//                 break
+//                 case 4:
+//                     alert('Aprel bahor');
+//                     break
+//                     case 5:
+//                         alert("May bahor");
+//                         break
+//                         case 6:
+//                             alert("Iyun yoz");
+//                             break
+//                             case 7:
+//                                 alert("Iyul yoz");
+//                                 break
+//                                 case 8:
+//                                     alert("Avgust yoz");
+//                                     break
+//                                     case 9:
+//                                         alert("Sentabr kuz");
+//                                         break
+//                                         case 10:
+//                                             alert("Oktabr kuz");
+//                                             break
+//                                             case 11:
+//                                                 alert("Noyabr kuz");
+//                                                 break
+//                                                 case 12:
+//                                                     alert("Dekabr qish");
+//                                                     break
+//                                 default :
+//                                 alert("Bunaqa oy va fasl  yuq")
+// }
+// let month = +prompt("Son kirit");
+// switch (month) {
+//     case 1:
+//         alert('Yanvar 31 kun');
+//         break
+//         case 2:
+//             alert('Fevral 29 kun');
+//             break
+//             case 3:
+//                 alert('Mart 31 kun');
+//                 break
+//                 case 4:
+//                     alert('Aprel 30 kun');
+//                     break
+//                     case 5:
+//                         alert("May 31 kun");
+//                         break
+//                         case 6:
+//                             alert("Iyun 30 kun");
+//                             break
+//                             case 7:
+//                                 alert("Iyul 31 kun");
+//                                 break
+//                                 case 8:
+//                                     alert("Avgust 31 kun");
+//                                     break
+//                                     case 9:
+//                                         alert("Sentabr 30 kun");
+//                                         break
+//                                         case 10:
+//                                             alert("Oktabr 31 kun");
+//                                             break
+//                                             case 11:
+//                                                 alert("Noyabr 30 kun");
+//                                                 break
+//                                                 case 12:
+//                                                     alert("Dekabr 31 kun");
+//                                                     break
+//                                 default :
+//                                 alert("Bunaqa oy va fasl  yuq")
+// }
+// let a =  5
+// let b = 5
+// let c = +prompt("Son kirit")
+// switch (c) {
+//     case 1:
+//         alert(a + b);
+//         break
+//         case 2:
+//             alert(a - b);
+//             break
+//             case 3:
+//                 alert(a / b);
+//                 break
+//                 case 4:
+//                     alert(a * b);
+//                     break
+//                     default :
+//                     alert('Xatolik yuz berdi')
+// }
+// let num = +prompt("Son kirit");
+// let kg = +prompt("killogram kirit")
+// switch (kg) {
+//     case 1:
+//         alert(kg)
+//         break
+//         case 2:
+//             alert(kg * 1000000)
+//             break
+//             case 3:
+//                 alert(kg * 10000)
+//                 break
+//                 case 4:
+//                     alert(kg * 1000)
+//                     break
+//                     case 5:
+//                         alert(kg * 100)
+//                         break
+//                         default:
+//                             alert("Boshqa son kirit")
+                    
+// }
+// let day = +prompt("Kunni kirit");
+// let month = +prompt("Oyni kirit");
+// switch (month) {
+//     case 1:
+//         console.log(day, "-yanvar");
+//         break;
+//         case 2:
+//             console.log(day, "-fevral");
+//             break
+//             case 3:
+//                 console.log(day, "-mart");
+//                     break
+//                     case 4:
+//                     console.log(day, "-aprel");
+//                     break
+//                     case 5:
+//                         console.log(day, "-may");
+//                         break
+//                         case 6:
+//                             console.log(day, "-iyun");
+//                             break
+//                             case 7:
+//                                 console.log(day, "-iyul");
+//                                 break
+//                                 case 8:
+//                                     console.log(day, "-avgust");
+//                                     break
+//                                     case 9:
+//                                         console.log(day, "-sentabr");
+//                                         break
+//                                         case 10:
+//                                             console.log(day, "-oktabr");
+//                                             break
+//                                             case 11:
+//                                                 console.log(day, "-noyabr");
+//                                                 break
+//                                                 case 12:
+//                                                     console.log(day, "-dekabr");
+//                                                     break
+//                                                     default:
+//                                                         alert("boshqa son kirit")
+// }
+4
